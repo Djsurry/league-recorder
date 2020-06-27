22cab42d51b30fe54ccc920e869b239b9873e3e7
David Surry <djsurry@gmail.com>
2020-06-26 21:59:21 -0400

    changed cmd

--- record_screen.py
+++ record_screen.py
@@ -1,6 +1,6 @@
 from subprocess import Popen, PIPE, STDOUT
 from os import path
-import time
+import time, datetime
 '''
 #########
 
@@ -11,7 +11,7 @@ RUNNING FTP SERVICE. brew services stop pure-ftpd
 
 
 
-record_cmd = 'ffmpeg -f gdigrab -framerate 10 -i title="League of Legends" {out}'
+record_cmd = 'ffmpeg -y -rtbufsize 100M -f gdigrab -framerate 30 -probesize 10M -draw_mouse 1 -i desktop -c:v libx264 -r 30 -preset ultrafast -tune zerolatency -crf 25 -pix_fmt yuv420p {out}'
 compress_cmd = 'ffmpeg -i {inp} -vcodec libx265 -crf 28 {out}'
 
 
@@ -21,11 +21,12 @@ class Recorder:
         self.process = None
         self.path = folder
 
-    def start(self, out):
-        self.out = out
-        self.process = Popen(record_cmd.format(out="temp.mp4").split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    def start(self):
+        
+        self.process = Popen(record_cmd.format(out=path.abspath(os.getcwd()) + "\\temp.mp4").split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 
     def stop(self):
+        name = f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}"
         if self.process is not None:
             resp = self.process.communicate(input=b'q')
         else:
@@ -34,9 +35,9 @@ class Recorder:
         if not path.exists("temp.mp4"):
             print('Something went wrong - Recording failed')
             return
-        p = Popen(compress_cmd.format(out=folder + self.out, inp="temp.mp4").split())
+        p = Popen(compress_cmd.format(out=folder + name, inp="temp.mp4").split())
         p.communicate()
-        if not path.exists(folder + self.out):
+        if not path.exists(folder + name):
             print("Something went wrong - Compression Failed")
             return
 
